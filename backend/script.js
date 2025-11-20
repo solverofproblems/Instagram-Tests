a41c53e8ca0037cf261b738969e4b82d7f824f5b
Carlos Eduardo de Lima <carloselima373@gmail.com>
2025-11-20 14:51:39 -0300

    Inserções

---
+++ backend/script.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import express from "express";
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+
+app.get('/webhook', (res,req) => {
+
+    const TOKEN = process.env.MEU_TOKEN_SECRETO;
+
+    const mode = req.query['hub.mode'];
+    const token_correto = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (token_correto === TOKEN){
+
+      return res.status(200).send(challenge)
+
+   } else {
+
+    return res.sendStatus(403);
+   }
+
+})
+
+app.listen(3000);
